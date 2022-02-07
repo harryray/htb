31eb096ce726e1e85002b27b00fe17b39eac0d76
Harry Ray <32261536+harryray@users.noreply.github.com>
2022-02-07 21:45:09 +0000

    Speech bubbles start

--- wp-content/themes/htb/assets/js/site.js
+++ wp-content/themes/htb/assets/js/site.js
@@ -1,62 +1,83 @@
 (function ($, root, undefined) {
 	
 	$(function () {
+
+		$(document).ready(function(){
 		
-		'use strict';
+			'use strict';
 
-		//smoothscroll
-		// Select all links with hashes
-		$('a[href*="#"]')
-		  // Remove links that don't actually link to anything
-		  .not('[href="#"]')
-		  .not('[href="#0"]')
-		  .click(function(event) {
-		    // On-page links
-		    if (
-		      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-		      && 
-		      location.hostname == this.hostname
-		    ) {
-		      // Figure out element to scroll to
-		      var target = $(this.hash);
-		      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		      // Does a scroll target exist?
-		      if (target.length) {
-		        // Only prevent default if animation is actually gonna happen
-		        event.preventDefault();
-		        $('html, body').animate({
-		          scrollTop: (target.offset().top + 1)
-		        }, 1000);
-		      }
-		    }
-		  });
+			//smoothscroll
+			// Select all links with hashes
+			$('a[href*="#"]')
+			  // Remove links that don't actually link to anything
+			  .not('[href="#"]')
+			  .not('[href="#0"]')
+			  .click(function(event) {
+			    // On-page links
+			    if (
+			      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+			      && 
+			      location.hostname == this.hostname
+			    ) {
+			      // Figure out element to scroll to
+			      var target = $(this.hash);
+			      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			      // Does a scroll target exist?
+			      if (target.length) {
+			        // Only prevent default if animation is actually gonna happen
+			        event.preventDefault();
+			        $('html, body').animate({
+			          scrollTop: (target.offset().top + 1)
+			        }, 1000);
+			      }
+			    }
+			  });
 
-		var controller = new ScrollMagic.Controller();
-		$('.block').each(function(){
+			var controller = new ScrollMagic.Controller();
+			$('.block').each(function(){
 
-				var container = $(this);
+					var container = $(this);
 
-				new ScrollMagic.Scene({triggerElement: $(this).find('.scroll-trigger')[0]})
-					.on("enter", function(event) {
-						container.addClass('animations-on');
-						
-						// Gifs
-						setTimeout(function(){
-							var canv = $(container).find('canvas');
-							canv.each(function(e){
-								var btn = $(this).parents('button');
-								btn.addClass('active');
-								this.click();
-								btn.attr('disabled', 'disabled');
-							});
-						},500);
-						// /Gifs
-					})
-					.addTo(controller);
+					new ScrollMagic.Scene({triggerElement: $(this).find('.scroll-trigger')[0]})
+						.on("enter", function(event) {
+							container.addClass('animations-on');
+							
+							// Gifs
+							setTimeout(function(){
+								var canv = $(container).find('canvas');
+								canv.each(function(e){
+									var btn = $(this).parents('button');
+									btn.addClass('active');
+									this.click();
+									btn.attr('disabled', 'disabled');
+								});
+							},500);
+							// /Gifs
+						})
+						.addTo(controller);
+			});
+
+			// Get speech bubbles, hide all text within them.
+			// Type out each string character by character, pressing "enter" skips to end of string and then to next string
+			function handleSpeechBubble(i, e) {
+				/* 
+				e = targeted element, should be speech-bubble
+				s = children of e, should be strings
+				*/
+				e = $(e)[0];
+				const s = e.children;
+
+				// get all strings. count all strings. 
+				// detect active string by number. 
+				// if active string not fully visible, "Enter" or "Down" skips to end.
+				// if active string is at end, hide active string, iterate number, run 
+				// function again to handle new string.
+				
+			}
+
+			$('.speech-bubble').each(handleSpeechBubble);
+
 		});
-
-		// Get speech bubbles, hide all text within them.
-		// Type out each string character by character, pressing "enter" skips to end of string and then to next string
 		
 	});
 	
