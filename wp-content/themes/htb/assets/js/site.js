6f3d9e916031b067b10b8feef0fbf4a5e086d291
Harry Ray <32261536+harryray@users.noreply.github.com>
2022-02-07 21:32:01 +0000

    redo

--- wp-content/themes/htb/assets/js/site.js
+++ wp-content/themes/htb/assets/js/site.js
@@ -31,69 +31,8 @@
 		    }
 		  });
 
-		// Header appear on scrollup
-		// Duplicate the header, give it a class of header--scroll
-		$("header").clone().appendTo("body").addClass('header--scroll');
-
-		 // Header nav
-		 $('.header__nav-toggle').click(function(){
-		 	$('.header__nav-toggle').toggleClass('active');
-		 	$('header .nav--mobile').fadeToggle(300);
-		 })
-
-		// use that as the clone to appear on scrollUp
-		var lastScrollTop = window.pageYOffset || $(window).scrollTop;
-		$(window).scroll(function(e) {  
-		    var scroll = window.pageYOffset || $(window).scrollTop();
-		    if(scroll > 300) {
-			    if (scroll > lastScrollTop) {
-			    	$('.header--scroll').removeClass('active');
-			    } else {
-			    	$('.header--scroll').addClass('active');
-			    }
-			} else {
-				$('.header--scroll').removeClass('active');
-			}
-		    lastScrollTop = scroll <= 0 ? 0 : scroll;
-		});
-
 		var controller = new ScrollMagic.Controller();
 		$('.block').each(function(){
-
-
-			// Image Text
-			if($(this).attr('data-block-name') === 'image-text') {
-
-				var container = $(this);
-
-				new ScrollMagic.Scene({triggerElement: $(this).find('.scroll-trigger')[0]})
-					.on("enter", function(event) {
-						container.addClass('animations-on');
-
-						// Gifs
-						setTimeout(function(){
-							var canv = $(container).find('canvas');
-							canv.each(function(){
-								var btn = $(this).parents('button');
-								btn.addClass('active');
-								this.click();
-								btn.attr('disabled', 'disabled');
-							});
-						},500);
-						// /Gifs
-
-						var visibleTimer = 0;
-
-						container.find('.image-text__text ul li').each(function(){
-							var listItem = $(this);
-							visibleTimer = visibleTimer + 333;
-							setTimeout(function(){
-								listItem.addClass("visible");
-							}, visibleTimer);
-						})
-					})
-					.addTo(controller);
-			} else {
 
 				var container = $(this);
 
@@ -114,36 +53,10 @@
 						// /Gifs
 					})
 					.addTo(controller);
-			}
-		});
-		
-		// Sliders
-
-		$('.games-carousel__slider').slick({
-			slidesToShow: 3.5,
-			autoplay: true,
-			infinite: false,
-			arrows: true,
-			dots: false,
-			nextArrow: $('.games-carousel__slider-next'),
-			prevArrow: $('.games-carousel__slider-prev'),
-			responsive: [
-				{
-					breakpoint: 992,
-					settings: {
-						slidesToShow: 1,
-					}
-				},
-			]
 		});
 
-		// Show submenu
-		$('li.menu-item-has-children > a').click(function(e){
-			e.preventDefault();
-			var li = $(this).parent();
-			$(li).toggleClass('active');
-			$(li).children('ul').slideToggle(600);
-		})
+		// Get speech bubbles, hide all text within them.
+		// Type out each string character by character, pressing "enter" skips to end of string and then to next string
 		
 	});
 	
--- wp-content/themes/htb/front-page.php
+++ wp-content/themes/htb/front-page.php
@@ -5,37 +5,22 @@ get_header();
 	<div id="primary" class="content-area homepage">
 		<main id="main" class="site-main">
 
-			<section class="homepage__section--one">
+			<section class="homepage__section homepage__section--one">
 				
-				<div class="homepage__tray tray">
-					
-					<p>Hey, how're you doing?</p>
-					<p>Fancy seeing you here.</p>
-					<p>Been caught snooping?</p>
-
-				</div>
-
 				<div>
+					
 
-					<div class="tile homepage__tile homepage__tile--spotify">
-
-						<div>
-							<h2>Need a playlist? I'm on it.</h2>
-							<!-- spotify logo in red -->
-							<svg xmlns="http://www.w3.org/2000/svg" height="168px" width="168px" version="1.1" viewBox="0 0 168 168">
-							 <path fill="#c60000" d="m83.996 0.277c-46.249 0-83.743 37.493-83.743 83.742 0 46.251 37.494 83.741 83.743 83.741 46.254 0 83.744-37.49 83.744-83.741 0-46.246-37.49-83.738-83.745-83.738l0.001-0.004zm38.404 120.78c-1.5 2.46-4.72 3.24-7.18 1.73-19.662-12.01-44.414-14.73-73.564-8.07-2.809 0.64-5.609-1.12-6.249-3.93-0.643-2.81 1.11-5.61 3.926-6.25 31.9-7.291 59.263-4.15 81.337 9.34 2.46 1.51 3.24 4.72 1.73 7.18zm10.25-22.805c-1.89 3.075-5.91 4.045-8.98 2.155-22.51-13.839-56.823-17.846-83.448-9.764-3.453 1.043-7.1-0.903-8.148-4.35-1.04-3.453 0.907-7.093 4.354-8.143 30.413-9.228 68.222-4.758 94.072 11.127 3.07 1.89 4.04 5.91 2.15 8.976v-0.001zm0.88-23.744c-26.99-16.031-71.52-17.505-97.289-9.684-4.138 1.255-8.514-1.081-9.768-5.219-1.254-4.14 1.08-8.513 5.221-9.771 29.581-8.98 78.756-7.245 109.83 11.202 3.73 2.209 4.95 7.016 2.74 10.733-2.2 3.722-7.02 4.949-10.73 2.739z"/>
-							</svg>
-						</div>
-						<ul>
-							<li><a target="_blank" href="https://open.spotify.com/playlist/6CzfU7lWSV4HXGZNqhrMS9?si=93996d6bdccf4308">Four Hundred and Thirty-Four Million People, All Eating Bread On The Salt Flats</a></li>
-							<li><a target="_blank" href="https://open.spotify.com/playlist/0KfHCKHN4Cbasbj5F3n3yC?si=e07a05baff8d41e7">Gonna FUNK your hole</a></li>
-							<li><a target="_blank" href="https://open.spotify.com/playlist/1t7F1tPjeuGdfM29BRzZT1?si=8ef38166867a40a5">DA BIG 1 (Hey Juan, You Dropped Your Shorts)</a></li>
-							<li><a target="_blank" href="https://open.spotify.com/playlist/3kuiuKxQJQjbaoX0otvx73?si=8efb61c790e14db0">I'm gonna PUNK my pants</a></li>
-							<li><a target="_blank" href="https://open.spotify.com/playlist/6LsVnuFVF0y6ueliwrfw9V?si=e47386e9b49e420c">I'm gonna cry myself to climax fully onto the underside of this pub table</a></li>
-							<li><a target="_blank" href="https://open.spotify.com/playlist/27NWuADiAVfgtNpkof3DcK?si=b37628a78b884673"><em>BANGERS</em> and mash</a></li>
-							<li><a target="_blank" href="https://open.spotify.com/playlist/3IYKUiM8S9Noqa6DwLXPdt?si=78e9393c751a467b">Oh, shit! Meaty!</a></li>
-							<li><a target="_blank" href="https://open.spotify.com/playlist/0wCzc7AOLrj3TCB0jQ4DJ1?si=58ba87d5bf6b4f24">The Beatles are good and also this is the chilled love one about them</a></li>
-						</ul>
+					<div class="speech-bubble">
+						<p class="statement">...</p>
+						<p class="statement">...</p>
+						<p class="statement">omg hi!</p>
+						<p class="statement">I must've forgotten to lock the door again, huh?</p>
+						<p class="statement">I didn't expect you to just barge in here, honestly...</p>
+						<p class="statement">But, no bother! Welcome!</p>
+						<p class="question">How are you?</p>
+						<p class="answer">Good! You?</p><!-- Make the conversation awkward. "Good to hear! I'm good thank you. Um..."-->
+						<p class="answer">I think I'm lost...</p><!-- if this is clicked, get trippy. "You are not lost, my child." -->
+						<p class="answer">Who are you?</p><!-- Dunno, do something here. Something funny. "I'm Harry! Harry the Bastard, of course! It's literally in the URL." -->
 					</div>
 
 				</div>
